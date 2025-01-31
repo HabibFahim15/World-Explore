f8a1cc3fa40041be8efefa50233e36661d4d7894
HabibFahim15 <habibfahim71@gmail.com>
2025-02-01 02:07:39 +0600

    about page complete and some update in home page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,40 @@
-
+import countryData from "../api/countryData.json";
 const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-    </div>
+    <section className="section-about container">
+      <h2 className="container-title">
+        Here are Interesting Facts
+        <br />
+        we're proud of
+      </h2>
+
+      <div className="gradient-cards">
+        {
+          countryData.map((country) => {
+            return (
+              <div className="card" key={country.id}>
+              <div className="container-card bg-blue-box">
+                <p className="card-title">{country.countryName}</p>
+                <p>
+                  <span className="card-description">Capital:</span>
+                  {country.capital}
+                </p>
+                <p>
+                  <span className="card-description">Population:</span>
+                  {country.population}
+                </p>
+                <p>
+                  <span className="card-description">Interesting Fact:</span>
+                  {country.interestingFact}
+                </p>
+              </div>
+            </div>
+            )
+          })
+        }
+       
+      </div>
+    </section>
   );
 };
 
