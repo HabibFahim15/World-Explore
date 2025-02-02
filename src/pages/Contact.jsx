83297deb5576840af517c179eabdd0221297990b
HabibFahim15 <habibfahim71@gmail.com>
2025-02-03 02:53:12 +0600

    contact us form are ready ..style are remain

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,34 @@
 
 const Contact = () => {
+
+  const handleFormSubmit = (formData) => {
+    console.log(formData.entries());
+    const formInputData = Object.fromEntries(formData.entries());
+    console.log(formInputData);
+  }
+
   return (
-    <div>
-      <h1>Contact Page</h1>
-    </div>
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+      <div className="contact-wrapper container">
+        <form action={handleFormSubmit} >
+        <input type="text" 
+        className="form-control"
+        required 
+        autoComplete="false"
+        placeholder="Enter Your Name"
+        name="username" 
+        />
+        <input className="form-control" type="email" required autoComplete="false" placeholder="Enter Your Email" name="email" />
+
+        <textarea className="form-control" name="message" required placeholder="Enter Your Message" autoComplete="false"  rows="10"></textarea>
+
+
+        <button type="submit" value={"send"}>Send</button>
+      </form>
+      </div>
+      
+    </section>
   );
 };
 
